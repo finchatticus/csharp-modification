2bf018130f2f4c2b6bd8e33a40aba238032c09f0
finchatticus <koziy.vladyslav@gmail.com>
2016-09-13 23:56:17 +0300

    Disassembling and modification CIL language file

---
+++ HelloProgram.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloProgram {
+	class Program {
+		static void Main(string[] args) {
+			int v = 1;
+			float v1 = 2f;
+			double v2 = 3;
+			string myMessage = "Hello CIL code!";
+			Console.WriteLine(myMessage);
+			Console.ReadLine();
+		}
+	
+	}
+}
